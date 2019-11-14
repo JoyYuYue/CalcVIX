5f36a7ea73fc038128419f6a72add59542d4e4d8
俞越Joy <yuyue@yuyueJoydeMacBook-Pro.local>
2019-11-14 16:58:22 +0800

    add detailed daily graph

--- data_concat.py
+++ data_concat.py
@@ -3,22 +3,16 @@ import glob
 
 path = r'shibor'  # use your path
 all_files = glob.glob(path + "/*.csv")[::-1]
-print(all_files)
 
 li = []
 for filename in all_files:
-    print(filename)
     df = pd.read_csv(filename, header=0)
     li.append(df)
 frame = pd.concat(li, axis=0)
-frame.columns= ['time','1D','1W','2W','1M','3M','6M','9M','1Y']
-frame.set_index('time', inplace=True)
-print(frame)
-frame.to_csv('shibor.csv')
 
-# d1 = pd.read_csv('raw_data/OptionCode.csv')
-# d1 = d1.drop(d1.columns[0], axis=1)
-# d1.columns = ['de_listed_date', 'maturity_date', 'option_type', 'strike_price', 'listed_date', 'sid']
-# result = pd.merge(frame, d1, how='left', on=['sid'])
-# result.set_index('trading_date', inplace=True)
-# result.to_csv('data_test.csv')
+d1 = pd.read_csv('raw_data/OptionCode.csv')
+d1 = d1.drop(d1.columns[0], axis=1)
+d1.columns = ['de_listed_date', 'maturity_date', 'option_type', 'strike_price', 'listed_date', 'sid']
+result = pd.merge(frame, d1, how='left', on=['sid'])
+result.set_index('trading_date', inplace=True)
+result.to_csv('data_test.csv')
--- graph.py
+++ graph.py
@@ -2,16 +2,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-result = pd.read_csv('to_daily.csv')
-true_vix = pd.read_csv('true_vix.csv',encoding='GBK')
-# true_vix['date'] = pd.to_datetime(true_vix[u'日期'])
+# use the commented code to generate to_daily.csv and true_vix.csv
+# result = pd.read_csv('Calculated_result.csv')
+# result['datetime']=pd.to_datetime(result['time'])
+# result.set_index(result['datetime'],inplace = True)
+# dayvix = result.resample('D').mean()
+# dayvix = dayvix.dropna()
+# dayvix.to_csv('to_daily.csv')
+#
+# true_vix = pd.read_csv('ivixx.csv',usecols = [u'日期',u'收盘价(元)'],encoding='GBK')
+# true_vix['date'] = pd.to_datetime(true_vix[u'日期'].copy())
 # true_vix.set_index(true_vix['date'],inplace=True)
 # true_vix = true_vix.reindex(pd.date_range('2015-02-10','2019-10-22')).fillna(method='ffill')
+# true_vix = true_vix.drop(columns = [u'日期'])
+# true_vix.columns = ['price','date']
+# true_vix.to_csv('true_vix.csv')
 
+result = pd.read_csv('to_daily.csv')
+true_vix = pd.read_csv('true_vix.csv')
 
 plt.title("Calculated VIX")
 plt.plot(result['datetime'], result['Calculated_VIX'], label='Calculated VIX')
 plt.plot(true_vix.index,true_vix['price'],label='true vix')
 plt.yticks(np.arange(0, 80, step=5))
 plt.legend()
+fig = plt.gcf()
+fig.set_size_inches(28.5, 10.5)
+fig.savefig('test2png.png', dpi=100)
 plt.show()
